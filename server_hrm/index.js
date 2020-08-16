6f990ec9e009c2c7380beb938f9b3e36d3b927c5
arii <anders.ariel@gmail.com>
2020-08-16 02:59:46 -0700

    using web socket as protocol is now working

--- server_hrm/index.js
+++ server_hrm/index.js
@@ -3,6 +3,9 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+//disable long polling
+io.set('transports', ['websocket']);
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -13,6 +16,14 @@ io.on('connection', function(socket){
   });
 });
 
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
